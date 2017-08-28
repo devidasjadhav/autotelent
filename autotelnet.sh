a1e32b99ff9c59c03314737d3f57675b49b188e6
devidasjadhav <freekepange@gmail.com>
2017-08-28 15:28:08 +0530

    Update autotelnet.sh
    making it more generic

--- autotelnet.sh
+++ autotelnet.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/expect -f
-#spawn telnet `fip`
-#set fip [exec $(echo `fip`) ]
 set timeout -1
 spawn telnet [lindex $argv 0]
-#spawn telnet $fip
-#expect -re "(\\c+) login:"
 expect -re "(\\w+) login: "
-send "Administrator\r"
-expect "{Administrator}=>"
+send "USERNAME\r"
+expect "Telnet Prompt"
 interact
